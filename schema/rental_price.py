978d67245c95523f8787985e808b032b95ec32a7
Ashish <dhakalashish18@gmail.com>
2025-04-16 10:39:23 +0545

    fix: field validator

--- schema/rental_price.py
+++ schema/rental_price.py
@@ -1,11 +1,19 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
 from typing import List
+
 
 class PickLocationCheckInput(BaseModel):
     pickup: List[str] = Field(description="Array of pickup locations")
 
+    @field_validator("pickup")
+    def validate_pickup(cls, value):
+        print("inside PickLocationCheckInput", value)
+        return value
+
+
 class PickDropLocationCheckInput(PickLocationCheckInput):
     dropoff: List[str] = Field(description="Array of drop off locations")
+
 
 class EstimateBusRentalPriceInput(PickDropLocationCheckInput):
     passengers: int = Field(description="Total number of passengers travelling")
